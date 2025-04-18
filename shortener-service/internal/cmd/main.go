cb7d63d5e9044e20503a3bdaab96647f1a942a6f
Alireza <alireza.rezapour.me@gmail.com>
2025-04-19 02:50:23 +0330

    shortener-service: Implement main server

--- shortener-service/internal/cmd/main.go
+++ shortener-service/internal/cmd/main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/shortener-service/internal/account/application/services"
+	"github.com/shortener-service/internal/account/dal/repositories"
+	"github.com/shortener-service/internal/account/interfaces/http"
+	"github.com/shortener-service/internal/infrastructure/scylladb"
+)
+
 func main() {
-	println("Nothing for now!")
+	gin.SetMode(gin.ReleaseMode)
+
+	r := gin.New()
+	r.Use(gin.Logger())
+
+	// -- Initialize the database connection --
+	connection := scylladb.NewScyllaDB([]string{"127.0.0.1:9042"}, "shortener")
+
+	// -- Initialize the account handler --
+	accountRepository := repositories.NewAccountRepository(connection)
+	accountService := services.NewAccountService(accountRepository)
+	http.SetupAccountRoutes(r, &http.AccountHandler{AccountService: accountService})
+
+	log.Println("🚀 Server running at http://0.0.0.0:3000")
+
+	for _, route := range r.Routes() {
+		log.Println("Registered route:", route.Method, route.Path)
+	}
+
+	err := r.Run("0.0.0.0:3000")
+	if err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+		return
+	}
 }
