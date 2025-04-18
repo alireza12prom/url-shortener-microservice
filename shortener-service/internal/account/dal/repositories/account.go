14ea60fdb31153d23ca1f68acd492369c58635b6
Alireza <alireza.rezapour.me@gmail.com>
2025-04-19 02:49:26 +0330

    shortener-service: Implement AccountRepository

--- shortener-service/internal/account/dal/repositories/account.go
+++ shortener-service/internal/account/dal/repositories/account.go
@@ -1,4 +1,30 @@
 package repositories
 
+import (
+	"github.com/shortener-service/internal/account/dal/mappers"
+	"github.com/shortener-service/internal/account/domain/entities"
+	"github.com/shortener-service/internal/infrastructure/scylladb"
+)
+
 type AccountRepository struct {
+	connection *scylladb.ScyllaDB
+}
+
+func NewAccountRepository(connection *scylladb.ScyllaDB) *AccountRepository {
+	return &AccountRepository{
+		connection: connection,
+	}
+}
+
+func (r *AccountRepository) Save(account *entities.AccountEntity) error {
+	query := "INSERT INTO accounts (id, username, email, password, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?)"
+
+	model := mappers.MapToAccountModel(account)
+
+	err := r.connection.Session.Query(query, model.ID, model.Username, model.Email, model.Password, model.CreatedAt, model.UpdatedAt).Exec()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
