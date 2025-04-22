14ea8596af75c436342d990e011a375acaf02d67
Alireza Rezapour <alireza.rezapour.me@gmail.com>
2025-04-23 00:37:27 +0330

    url-shortener: Implement account repository methods for user retrieval and uniqueness checks

--- shortener-service/internal/account/dal/repositories/account.go
+++ shortener-service/internal/account/dal/repositories/account.go
@@ -1,7 +1,9 @@
 package repositories
 
 import (
+	"github.com/gocql/gocql"
 	"github.com/shortener-service/internal/account/dal/mappers"
+	"github.com/shortener-service/internal/account/dal/models"
 	"github.com/shortener-service/internal/account/domain/entities"
 	"github.com/shortener-service/internal/infrastructure/scylladb"
 )
@@ -17,7 +19,7 @@ func NewAccountRepository(connection *scylladb.ScyllaDB) *AccountRepository {
 }
 
 func (r *AccountRepository) Save(account *entities.AccountEntity) error {
-	query := "INSERT INTO accounts (id, username, email, password, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?)"
+	query := "INSERT INTO account (id, username, email, password, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?)"
 
 	model := mappers.MapToAccountModel(account)
 
@@ -27,4 +29,42 @@ func (r *AccountRepository) Save(account *entities.AccountEntity) error {
 	}
 
 	return nil
+}
+
+func (r *AccountRepository) GetByUserId(userId string) (*entities.AccountEntity, error) {
+	query := "SELECT * FROM users WHERE id = ? LIMIT 1"
+
+	var result models.AccountModel
+	err := r.connection.Session.Query(query, userId).Consistency(gocql.One).Scan(&result)
+	if err != nil {
+		return nil, err
+	}
+
+	return mappers.MapToAccountDomain(&result), nil
+}
+
+func (r *AccountRepository) IsEmailUnique(email string) (bool, error) {
+	query := "SELECT COUNT(*) FROM account_by_email WHERE email = ? LIMIT 1"
+
+	var result int
+
+	err := r.connection.Session.Query(query, email).Consistency(gocql.One).Scan(&result)
+	if err != nil {
+		return false, err
+	}
+
+	return result == 0, nil
+}
+
+func (r *AccountRepository) IsUsernameUnique(email string) (bool, error) {
+	query := "SELECT COUNT(*) FROM account_by_username WHERE username = ? LIMIT 1"
+
+	var result int
+
+	err := r.connection.Session.Query(query, email).Consistency(gocql.One).Scan(&result)
+	if err != nil {
+		return false, err
+	}
+
+	return result == 0, nil
 }
