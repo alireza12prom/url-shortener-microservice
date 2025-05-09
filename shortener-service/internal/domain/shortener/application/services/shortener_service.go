29eac170a634a653f2ecb9330c25201db9c8512a
Alireza Rezapour <alireza.rezapour.me@gmail.com>
2025-05-09 17:32:56 +0330

    shortener-service: implement shortener service logic

--- shortener-service/internal/domain/shortener/application/services/shortener_service.go
+++ shortener-service/internal/domain/shortener/application/services/shortener_service.go
@@ -1,17 +1,22 @@
 package services
 
 import (
+	"github.com/google/uuid"
 	"github.com/shortener-service/internal/domain/shortener/application/commands"
+	"github.com/shortener-service/internal/domain/shortener/domain/entities"
 	"github.com/shortener-service/internal/domain/shortener/domain/interfaces"
+	domain_services "github.com/shortener-service/internal/domain/shortener/domain/services"
 )
 
 type ShortenerService struct {
-	shortURLRepo interfaces.ShortURLRepository
+	shortURLRepo           interfaces.ShortURLRepository
+	hashReservationService interfaces.HashReservationService
 }
 
 func NewShortenerService(shortURLRepo interfaces.ShortURLRepository) *ShortenerService {
 	return &ShortenerService{
-		shortURLRepo: shortURLRepo,
+		shortURLRepo:           shortURLRepo,
+		hashReservationService: domain_services.NewHashReservationService(),
 	}
 }
 
@@ -19,6 +24,23 @@ func (s *ShortenerService) ShortenCustom(command *commands.ShortenCustomCommand)
 	*commands.ShortenCustomCommandOutput,
 	error,
 ) {
+	entity, err := entities.NewShortURL(
+		uuid.New().String(),
+		command.UserID,
+		command.Hash,
+		command.Endpoint,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO: check hash is unique
+
+	err = s.shortURLRepo.Save(&entity)
+	if err != nil {
+		return nil, err
+	}
+
 	return &commands.ShortenCustomCommandOutput{}, nil
 }
 
@@ -26,5 +48,22 @@ func (s *ShortenerService) ShortenRandom(command *commands.ShortenRandomCommand)
 	*commands.ShortenRandomCommandOutput,
 	error,
 ) {
+	entity, err := entities.NewShortURL(
+		uuid.New().String(),
+		command.UserID,
+		s.hashReservationService.Reserve(command.Length),
+		command.Endpoint,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO: check hash is unique
+
+	err = s.shortURLRepo.Save(&entity)
+	if err != nil {
+		return nil, err
+	}
+
 	return &commands.ShortenRandomCommandOutput{}, nil
 }
