e83091fe39c201f3458cddae43624b2953268568
Alireza Rezapour <alireza.rezapour.me@gmail.com>
2025-05-30 19:07:37 +0330

    redirector-service: implement redirector service

--- redirector-service/internal/domain/redirector/application/services/redirector_service.go
+++ redirector-service/internal/domain/redirector/application/services/redirector_service.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"github.com/shortener-service/internal/common/exceptions"
 	"github.com/shortener-service/internal/domain/redirector/application/commands"
 	"github.com/shortener-service/internal/domain/redirector/domain/interfaces"
 )
@@ -13,5 +14,16 @@ func (Self *RedirectorService) Redirect(command commands.RedirectCommand) (
 	*commands.RedirectCommandOutput,
 	error,
 ) {
-	return nil, nil
+	shortURL, err := Self.shortURLRepo.GetByHash(command.Hash)
+	if err != nil {
+		return nil, err
+	}
+
+	if shortURL == nil {
+		return nil, exceptions.NewBusinessException(exceptions.ShortURLNotFound, nil)
+	}
+
+	return &commands.RedirectCommandOutput{
+		Endpoint: shortURL.Endpoint.GetValue(),
+	}, nil
 }
--- redirector-service/internal/domain/redirector/domain/interfaces/short_url_repository.go
+++ redirector-service/internal/domain/redirector/domain/interfaces/short_url_repository.go
@@ -3,5 +3,5 @@ package interfaces
 import "github.com/shortener-service/internal/domain/redirector/domain/entities"
 
 type ShortURLRepository interface {
-	GetByHash(hash string) *entities.ShortURL
+	GetByHash(hash string) (*entities.ShortURL, error)
 }
