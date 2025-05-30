ae15a515c33902d9ca3c1b404551b6e964290eb6
Alireza Rezapour <alireza.rezapour.me@gmail.com>
2025-05-30 18:27:33 +0330

    redirector-service: add short_url entity

--- redirector-service/go.mod
+++ redirector-service/go.mod
@@ -1,3 +1,5 @@
-module github.com/alireza12prom/url-shortener-microservice/redirector-service
+module github.com/shortener-service
 
 go 1.23.5
+
+require github.com/google/uuid v1.6.0 // indirect
---
+++ redirector-service/internal/domain/redirector/domain/entities/short_url.go
@@ -0,0 +1,49 @@
+package entities
+
+import (
+	"time"
+
+	ValueObjects "github.com/shortener-service/internal/domain/redirector/domain/value-objects"
+)
+
+type ShortURL struct {
+	ID             *ValueObjects.ID
+	UserID         *ValueObjects.ID
+	Hash           *ValueObjects.Hash
+	IsActive       bool
+	Endpoint       *ValueObjects.Endpoint
+	CreatedAt      time.Time
+	LastAccessedAt time.Time
+}
+
+func NewShortURL(id, userId, hash, endpoint string) (ShortURL, error) {
+	ID, err := ValueObjects.NewID(id)
+	if err != nil {
+		return ShortURL{}, err
+	}
+
+	UserID, err := ValueObjects.NewID(userId)
+	if err != nil {
+		return ShortURL{}, err
+	}
+
+	Hash, err := ValueObjects.NewHash(hash)
+	if err != nil {
+		return ShortURL{}, err
+	}
+
+	Endpoint, err := ValueObjects.NewEndpoint(endpoint)
+	if err != nil {
+		return ShortURL{}, err
+	}
+
+	return ShortURL{
+		ID:             ID,
+		UserID:         UserID,
+		Hash:           Hash,
+		IsActive:       true,
+		Endpoint:       Endpoint,
+		CreatedAt:      time.Now(),
+		LastAccessedAt: time.Now(),
+	}, nil
+}
