96d9be86a889722651c92ddf2984fc004411a56e
Alireza Rezapour <alireza.rezapour.me@gmail.com>
2025-06-20 05:27:01 +0330

    shortener-service: update short_url_created event model

--- shortener-service/internal/domain/shortener/domain/events/short_url_created.go
+++ shortener-service/internal/domain/shortener/domain/events/short_url_created.go
@@ -8,14 +8,24 @@ import (
 	"github.com/shortener-service/internal/lib"
 )
 
-type ShortURLCreatedPayload struct{}
+type ShortURLCreatedPayload struct {
+	ID       string `json:"id"`
+	UserID   string `json:"user_id"`
+	Hash     string `json:"hash"`
+	Endpoint string `json:"endpoint"`
+}
 
 func NewShortURLCreatedEvent(entity *entities.ShortURL) *lib.Event {
 	return &lib.Event{
-		ID:            entity.ID.GetValue(),
-		Name:          "account_created",
-		Context:       "account",
-		Payload:       ShortURLCreatedPayload{},
+		ID:      entity.ID.GetValue(),
+		Name:    "short_url_created",
+		Context: "shortener",
+		Payload: ShortURLCreatedPayload{
+			ID:       entity.ID.GetValue(),
+			UserID:   entity.UserID.GetValue(),
+			Hash:     entity.Hash.GetValue(),
+			Endpoint: entity.Endpoint.GetValue(),
+		},
 		DateTime:      time.Now().Format(time.RFC3339),
 		CorrelationID: uuid.New().String(),
 	}
