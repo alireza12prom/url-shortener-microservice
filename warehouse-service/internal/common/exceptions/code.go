6d70b64149a6d0128385749f3abcf6bdebe41934
Alireza Rezapour <alireza.rezapour.me@gmail.com>
2025-07-05 22:07:46 +0330

    warehouse-service: add a new type business exception

--- warehouse-service/internal/common/exceptions/code.go
+++ warehouse-service/internal/common/exceptions/code.go
@@ -3,10 +3,7 @@ package exceptions
 type ErrorCode string
 
 const (
-	UsernameAlreadyTaken    ErrorCode = "USERNAME_ALREADY_TAKEN"
-	WrongPasswordProvided   ErrorCode = "WRONG_PASSWORD_PROVIDED"
-	WrongAuthenticationPack ErrorCode = "WRONG_AUTHENTICATION_PACK"
-	LoginFailed             ErrorCode = "LOGIN_FAILED"
-	ValidationFailed        ErrorCode = "VALIDATION_FAILED"
-	DatabaseFailer          ErrorCode = "DATABASE_FAILER"
+	ValidationFailed   ErrorCode = "VALIDATION_FAILED"
+	DatabaseFailer     ErrorCode = "DATABASE_FAILER"
+	InvalidCommandType ErrorCode = "INVALID_COMMAND_TYPE"
 )
